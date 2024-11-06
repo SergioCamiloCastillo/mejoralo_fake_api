6e024ecc1a63dde965da35a77bdefec65807c802
SergioCamiloCastillo <sergiocastillo7398@gmail.com>
2024-11-06 11:44:35 -0500

    feature: configure appa

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const PORT = 8001;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Simulación de base de datos (esto debería ser reemplazado con una base de datos real)
